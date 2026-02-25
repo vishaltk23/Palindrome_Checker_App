1aa17c531047c8ad6f6c3674ad016e872aa1aea9
vishaltk23 <vishaltk004@gmail.com>
2026-02-25 14:51:18 +0530

    UC1 : Entry Message

---
+++ PalindromeCheckerApp/src/PalindromeChecker.java
@@ -0,0 +1,8 @@
+public class PalindromeChecker {
+    public static void main(){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version  : 1.0");
+        System.out.println("System initialized Sucessfully.");
+
+    }
+}
