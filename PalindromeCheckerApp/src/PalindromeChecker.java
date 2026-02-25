f5946345c119010f19c61c12cd33ee87fa9631c5
vishaltk23 <vishaltk004@gmail.com>
2026-02-25 15:52:50 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindromeCheckerApp/src/PalindromeChecker.java
+++ PalindromeCheckerApp/src/PalindromeChecker.java
@@ -1,8 +1,29 @@
+
 public class PalindromeChecker {
-    public static void main(){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version  : 1.0");
-        System.out.println("System initialized Sucessfully.");
+
+
+    public static void main(String[] args) {
+
+
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Output (MATCHES condition exactly)
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
 
     }
+
+
+
+
 }
